afbb425284ad855be7c9e6a191307fd3530e5273
Brody School Account <brody.mouw@jacks.sdstate.edu>
2021-01-28 14:29:59 -0600

    Routes page commit and bootstrap injection

---
+++ WellnessClimbingWall/Models/Route.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WellnessClimbingWall.Models
+{
+    public class Route
+    {
+        public int RouteId { get; set; }
+        public string Grade { get; set; }
+        public string Color { get; set; }
+        public int Rope { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public string Location { get; set; }
+        public string Setter { get; set; }
+
+    }
+}
