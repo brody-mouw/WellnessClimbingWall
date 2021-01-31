422e7f124366f4aaffe91e9b5bdc82afdaf526ed
Anthony Girton <cittenlover@outlook.com>
2021-01-31 16:20:24 -0600

    More login progress

--- WellnessClimbingWall/Controllers/LoginController.cs
+++ WellnessClimbingWall/Controllers/LoginController.cs
@@ -6,20 +6,6 @@ namespace WellnessClimbingWall.Controllers
 {
     public class LoginController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
-        public string Index()
-        {
-            return "This is my default action...";
-        }
-
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }
+       
     }
 }
--- WellnessClimbingWall/Models/Login.cs
+++ WellnessClimbingWall/Models/Login.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WellnessClimbingWall.Models
+{
+    public class Login
+    {
+        public string user { get; set; }
+        public string pass { get; set; }
+
+    }
+}
