05a497161d47806d73ebecd34ad726dfbeae207f
Anthony Girton <cittenlover@outlook.com>
2021-02-04 09:46:51 -0600

    Linked login page to be accessable

--- WellnessClimbingWall/Controllers/LoginController.cs
+++ WellnessClimbingWall/Controllers/LoginController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WellnessClimbingWall.Models;
-using System.Text.Encodings.Web;
 
 namespace WellnessClimbingWall.Controllers
 {
     public class LoginController : Controller
     {
-       
+        public ViewResult LoginView()
+        {
+            Login login = new Login();
+
+            return View(login);
+        }
+
     }
 }
