ee3094db7246b96e43e6fac7fd4bf1d70de56204
Anthony Girton <cittenlover@outlook.com>
2021-02-17 17:22:00 -0600

    Everything working

--- WellnessClimbingWall/Startup.cs
+++ WellnessClimbingWall/Startup.cs
@@ -26,6 +26,7 @@ namespace WellnessClimbingWall
         {
             services.AddControllersWithViews();
             services.AddScoped<IRouteRepository, MockRouteRepository>();
+            services.AddScoped<IPatronRepository, MockPatronRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
