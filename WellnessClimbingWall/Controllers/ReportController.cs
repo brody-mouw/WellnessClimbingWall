bcf0f8bbebbced8d0045ca85124b6ff66dc26aa8
Allison Girton <AllisonCGrimm@gmail.com>
2021-03-31 16:26:43 -0500

    Implemented what I could of the excel exporting

--- WellnessClimbingWall/Controllers/ReportController.cs
+++ WellnessClimbingWall/Controllers/ReportController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,18 @@ namespace WellnessClimbingWall.Controllers
             patronListViewModel.Patrons = _patronRepository.AllPatrons;
 
             return View(patronListViewModel);
+        }
+
+        public IActionResult Csv()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("Name, Time in, Time out");
+            foreach(var name in Patron)
+            {
+                builder.AppendLine($"{name.name},{name.timein},{name.timeout}");
+            }
+
+            return File(Encoding.UTF8.GetBytes(builder.ToString()),"text.csv","Report.csv");
         }
     }
 }
