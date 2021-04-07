31e504d29995964ef01082100e8906c824c34165
brody-mouw <brody.mouw@jacks.sdstate.edu>
2021-04-07 16:17:41 -0500

    Add Visit to DB

---
+++ WellnessClimbingWall/Migrations/20210407211542_changeVisit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WellnessClimbingWall.Migrations
+{
+    public partial class changeVisit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Patron",
+                keyColumn: "ID",
+                keyValue: 112233);
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Patron");
+
+            migrationBuilder.CreateTable(
+                name: "Visit",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Certifications = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    timeIn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    timeOut = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Visit", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Visit",
+                columns: new[] { "ID", "Certifications", "Name", "timeIn", "timeOut" },
+                values: new object[] { 112233, "Belay", "Chester", new DateTime(2021, 4, 7, 16, 15, 41, 268, DateTimeKind.Local).AddTicks(8024), new DateTime(2021, 4, 7, 16, 15, 41, 272, DateTimeKind.Local).AddTicks(2930) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Visit");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Patron",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "Patron",
+                columns: new[] { "ID", "Certifications", "Discriminator", "Name" },
+                values: new object[] { 112233, "Belay", "Visit", "Chester" });
+        }
+    }
+}
---
+++ WellnessClimbingWall/Models/VisitRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WellnessClimbingWall.Data;
+
+namespace WellnessClimbingWall.Models
+{
+    public class VisitRepository : IVisitRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public VisitRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Visit> AllVisits
+        {
+            get
+            {
+                return _appDbContext.Visit;
+            }
+        }
+
+        public Visit GetVisitById(int ID)
+        {
+            return _appDbContext.Visit.FirstOrDefault(p => p.ID == ID);
+        }
+    }
+}
